29f94cdfdfcae03a78706f0867882c46969597ff
Orit Kozolin <orit@nachum.co.il>
2020-05-04 12:49:11 +0300

    fix - indexroute is being called now although index.htm exist

--- routes/index.js
+++ routes/index.js
@@ -7,40 +7,41 @@ const Cache = require("../src/cache");
 const bookmarksParse = require("../src/bookmarksParse");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  // const bookmarks = await bookmarksParse();
-  // const getData = async () => {
-  //   let response,data
-  //   let pages = [];
-  //   for (let link of bookmarks) {
-  //     // console.log("link", link);
-  //     let { url, title } = link;
-  //     response = await axios.get(url);
-  //     data = response.data;
-  //     pages.push({ url, title, content: data });
-  //   }
-  //   return pages;
-  // };
+router.get("/", async function (req, res, next) {
+  const bookmarks = await bookmarksParse();
 
-  // console.log("inside index/get");
+  const getData = async () => {
+    let response,
+      data,
+      counter = 1;
+    let pages = [];
+    for (let link of bookmarks) {
+      // console.log("link", link, counter++);
+      let { url, title } = link;
+      response = await axios.get(url);
+      data = response.data;
+      pages.push({ url, title, pageContent: data });
+    }
+    return pages;
+  };
+
   // if (!Cache.has("pages") || Cache.isExpired("pages", 7)) {
   //   const response = await getData();
   //   console.log("response:");
-  //     Cache.set("pages", response);
+  //   Cache.set("pages", response);
   // }
 
   // res.locals.pages = Cache.get("pages");
   // console.log("res.locals", res.locals);
-  console.log("inside index-get route, __dirname", __dirname);
-  // res.send("inside index-get route");
+
   res.sendFile(
-    "home.html",
+    "index.html",
     { root: path.join(__dirname, "../public") },
     function (err) {
       if (err) {
         next(err);
       } else {
-        console.log("Sent file:");
+        console.log("Sent file");
       }
     }
   );
