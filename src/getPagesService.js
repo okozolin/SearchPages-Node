831f378669b1edcbd6747b3f1ea3d77138203ae8
Orit Kozolin <orit@nachum.co.il>
2020-05-18 15:25:16 +0300

    create custom read stream for jason file, and parse it while streaming. Use async stream iteration for await loop

--- src/getPagesService.js
+++ src/getPagesService.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const fs = require("fs");
-const { access, readFile } = fs.promises;
+const { access } = fs.promises;
 const { Readable, Transform } = require("stream");
 
 const path = require("path");
@@ -9,7 +9,7 @@ const bookmarksParse = require("./bookmarksParse");
 const publicDir = path.join(__dirname, "../public");
 const filePath = path.join(publicDir, "pagesstream.json");
 
-// STREAMS
+// CUSTOM STREAMS
 // ---------------------------------
 let pagesStream = new Readable({
   objectMode: true,
@@ -23,15 +23,10 @@ const stringifyTransform = new Transform({
     callback();
   },
 });
-const parseTransform = new Transform({
-  // writableObjectMode: true,
-  transform(chunk, encoding, callback) {
-    this.push(JSON.parse(chunk) + "\n");
-    callback();
-  },
-});
-
 // -------------
+
+// HELPER FUNCTIONS
+// ---------------------------------------
 const pagesFileExist = (filepath) => {
   return new Promise((resolve, reject) => {
     access(filepath, fs.constants.R_OK | fs.constants.W_OK)
@@ -46,27 +41,32 @@ const pagesFileExist = (filepath) => {
   });
 };
 
+async function processLineByLine() {
+  let rl = require("readline").createInterface({
+    input: fs.createReadStream(filePath),
+  });
+  let counter = 1;
+  let pages = [];
+
+  for await (const line of rl) {
+    console.log("line: ", counter++);
+    pages.push(JSON.parse(line));
+  }
+  return pages
+}
+
+//------------------------------------------
 const getPages = async () => {
   const exist = await pagesFileExist(filePath);
   console.log("pagesFileExist", exist);
 
   let pages = [];
   if (exist) {
-    // const src = fs.createReadStream(filePath, { encoding: "utf8" });
-    // src.pipe(parseTransform);
-    let counter = 1;
-    const file = await readFile(filePath, "utf8");
-    const fileSplit = file.split("\n");
-    fileSplit.pop(); //remove the last item , it is redundant
-
-    for (let link of fileSplit) {
-      page = JSON.parse(link);
-      console.log(counter++);
-      // data = response.data;
-      pages.push(page);
-    }
+    pages = await processLineByLine();
     return pages;
-  } else {
+  } 
+  else 
+  {
     let response, data;
     let counter = 1;
 
@@ -76,17 +76,13 @@ const getPages = async () => {
       let { url, title } = link;
       response = await axios.get(url);
       console.log(counter++);
-      // data = response.data;
       pages.push({ url, title, pageContent: data });
       pagesStream.push({ url, title, pageContent: response.data });
     }
-    pagesStream.push(null);
-    console.log("pages.length: ", pages.length);
-    console.log("pagesStream.length: ", pagesStream.length);
+    
+    pagesStream.push(null); // signal end of stream
     const writePagesToFileStream = fs.createWriteStream(filePath);
     pagesStream.pipe(stringifyTransform).pipe(writePagesToFileStream);
-
-    // await savePagesToFile(filePath, pages);
     return pages;
   }
 };
